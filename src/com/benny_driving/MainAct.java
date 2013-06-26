897b98e5e00819cee78dd50c8fb7ecd28d454b80
doubleleewu <doubleleewu@hotmail.com>
2013-06-26 16:16:58 +0800

    test

--- src/com/benny_driving/MainAct.java
+++ src/com/benny_driving/MainAct.java
@@ -17,7 +17,7 @@ public class MainAct extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-		//wulili
+		//wulilidddd
 	}
 
 }
